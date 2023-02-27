e52fe44cb13b1f06eaf5d07590ac256b7371bbba
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-02-27 21:35:41 +0900

    [Feat] BlogPost 컴포넌트 구현

--- src/Components/Common/BlogPost.tsx
+++ src/Components/Common/BlogPost.tsx
@@ -1,5 +1,60 @@
+import styled from "styled-components";
+import { IoHeartCircle } from "react-icons/io5";
+import { MdOutlineInsertComment } from "react-icons/md";
 import "./Style/BlogPost.css";
 
+interface SpanStyledProps {
+  usage: string;
+}
+
+interface IconStyledProps {
+  fontColor: string;
+  iconColor: string;
+  iconSize: string;
+}
+//PostData Type 미리 지정
+interface PostProps {
+  memberId: number;
+  blogId: number;
+  titleImageUrl: string;
+  blogTitle: string;
+  blogPreview: string;
+  createdAt: string;
+  writer: string;
+  commentCount: number;
+  likeCount: number;
+}
+
+const PostContent = styled.span<SpanStyledProps>`
+  font-size: ${(props) =>
+    props.usage === "title"
+      ? "var(--font-xxl)"
+      : props.usage === "info"
+      ? "var(--font-xs)"
+      : "var(--font-md)"};
+  color: ${(props) =>
+    props.usage === "writer" ? "var(--fc-500)" : "var(--fc-300)"};
+  font-weight: ${(props) =>
+    props.usage === "title"
+      ? "var(--fw-bold)"
+      : props.usage === "writer"
+      ? "var(--fw-bold)"
+      : "var(--fw-midium)"};
+  cursor: pointer;
+`;
+
+const IconContainer = styled.div<IconStyledProps>`
+  display: flex;
+  align-items: center;
+  color: ${(props) => props.fontColor};
+  font-size: var(--font-sm);
+
+  svg {
+    font-size: ${(props) => props.iconSize};
+    color: ${(props) => props.iconColor};
+    margin-right: 2px;
+  }
+`;
 const BlogPost = () => {
   return (
     <>
@@ -13,15 +68,34 @@ const BlogPost = () => {
             />
           </div>
           <div className="BlogPostBody_Container">
-            <h1>title</h1>
-            <p>content</p>
+            <div className="BlogPostBodyTitle_Container">
+              <PostContent usage="title">title</PostContent>
+            </div>
+            <div className="BlogPostBodyContent_Container">
+              <PostContent usage="content">content</PostContent>
+            </div>
             <div className="BlogPostInfo_Container">
-            <span>2022년 02월 28일</span>
-            <span>0개 댓글</span>
+              <PostContent usage="info">2022년 02월 28일</PostContent>
+              <IconContainer
+                fontColor="var(--fc-300)"
+                iconColor="var(--fc-300)"
+                iconSize="var(--font-md)"
+              >
+                <MdOutlineInsertComment className="Comment_icon" />
+                12
+              </IconContainer>
             </div>
           </div>
           <div className="BlogPostWriter_Container">
-            <span>글쓴이</span>
+            <PostContent usage="writer">글쓴이</PostContent>
+            <IconContainer
+              fontColor="var(--fc-500)"
+              iconColor="var(--blue-400)"
+              iconSize="var(--font-xl)"
+            >
+              <IoHeartCircle className="Heart_icon" />
+              12
+            </IconContainer>
           </div>
         </div>
       </div>
