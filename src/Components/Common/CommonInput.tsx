43f087372efd43b43d71c34d4372eee36b3f394a
limdumb <dlaruddls@nate.com>
2023-02-27 16:30:57 +0900

    [Feat] 공용 인풋 생성

---
+++ src/Components/Common/CommonInput.tsx
@@ -0,0 +1,48 @@
+import { ChangeEventHandler } from "react";
+import styled from "styled-components";
+import "./Style/commonInput.css";
+
+interface InputProps {
+  width: string;
+  height: string;
+  value: string;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+  type?: "text" | "password" | "email";
+  placeholder: string;
+  radious?: string;
+}
+
+interface Props extends InputProps {
+  width: string;
+  height: string;
+  label: string;
+  radious: string;
+}
+
+const Input = styled.input<InputProps>`
+  width: ${(props) => (props.width ? props.width : {})};
+  height: ${(props) => (props.height ? props.height : {})};
+  border: 1px solid gray;
+  border-radius: ${(props) => (props.radious ? props.radious : "none")};
+  padding-left: 5px;
+  ::placeholder {
+    color: #999;
+  }
+`;
+
+export default function CommonInput(props: Props) {
+  return (
+    <div className="Input_Container">
+      <label className="Input_Label">{props.label}</label>
+      <Input
+        radious={props.radious}
+        placeholder={props.placeholder}
+        type={props.type}
+        value={props.value}
+        onChange={props.onChange}
+        width={props.width}
+        height={props.height}
+      />
+    </div>
+  );
+}
