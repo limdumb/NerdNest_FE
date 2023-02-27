3524ec61497fe8c530531a20cd05a53aa180afe2
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-02-27 18:17:13 +0900

    [Feat] 로그인 헤더 컴포넌트 생성

--- src/Components/Common/Header.tsx
+++ src/Components/Common/Header.tsx
@@ -2,8 +2,11 @@ import { TiPencil } from "react-icons/ti";
 import { RiSearchLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
 import "./Style/Header.css";
+import { useState } from "react";
+import DropDownTab from "../DropDownTab";
 
 export default function Header() {
+  const [isLogin, setIsLogin] = useState(true);
   return (
     <div className="Header_Wrapper">
       <div className="Header_Container">
@@ -18,13 +21,26 @@ export default function Header() {
             <RiSearchLine className="Search icon" />
           </Link>
           <div className="Header_Login_SignUp_Container">
-            <Link to="/login">
-              <div className="Header_Login btn">로그인</div>
-            </Link>
-            &nbsp;/&nbsp; 
-            <Link to="/signup">
-              <div className="Header_SignUp btn">회원가입</div>
-            </Link>
+            {isLogin ? (
+              <>
+                <img
+                  className="Header_Member_Image"
+                  src="https://images.unsplash.com/photo-1676824469794-9d8deeaf1f2c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+                  alt="memberImage"
+                />
+                <DropDownTab />
+              </>
+            ) : (
+              <>
+                <Link to="/login">
+                  <div className="Header_Login btn">로그인</div>
+                </Link>
+                &nbsp;/&nbsp;
+                <Link to="/signup">
+                  <div className="Header_SignUp btn">회원가입</div>
+                </Link>{" "}
+              </>
+            )}
           </div>
         </div>
       </div>
---
+++ src/Components/DropDownTab.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { RxTriangleDown } from "react-icons/rx";
+import "./Style/DropDownTab.css";
+
+const DropDownTab = () => {
+  const [isTab, setIsTab] = useState(false);
+  return (
+    <>
+      <RxTriangleDown
+        className="Profile_Tab icon"
+        onClick={() => setIsTab(!isTab)}
+      />
+      {isTab ? (
+        <ul className="DropDownTab_Container">
+          <li className="DropDownTab_Logout Menu">로그아웃</li>
+          <li className="DropDownTab_MyBlog Menu">내 블로그</li>
+        </ul>
+      ) : null}
+    </>
+  );
+};
+
+export default DropDownTab;
