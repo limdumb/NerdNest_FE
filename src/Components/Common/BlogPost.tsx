35df99a0474fa5674d541657d345b0665bbd8968
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-02-28 10:27:06 +0900

    [MISC] BlogPost Props Data 연결

--- src/Components/Common/BlogPost.tsx
+++ src/Components/Common/BlogPost.tsx
@@ -5,7 +5,7 @@ import "./Style/BlogPost.css";
 
 interface SpanStyledProps {
   usage: string;
-  mt?: string;
+  marginTop?: string;
 }
 
 interface IconStyledProps {
@@ -41,7 +41,7 @@ const PostContent = styled.span<SpanStyledProps>`
       : props.usage === "writer"
       ? "var(--fw-bold)"
       : "var(--fw-midium)"};
-  margin-top: ${(props) => props.mt};
+  margin-top: ${(props) => props.marginTop};
   cursor: pointer;
 `;
 const IconContainer = styled.div<IconStyledProps>`
@@ -56,7 +56,7 @@ const IconContainer = styled.div<IconStyledProps>`
     margin-right: 2px;
   }
 `;
-const BlogPost = () => {
+const BlogPost = ({ post }: { post: PostProps }) => {
   return (
     <>
       <header className="BlogPost_Wrapper">
@@ -64,35 +64,35 @@ const BlogPost = () => {
           <div className="BlogPostImage_Container">
             <img
               className="BlogPostImage"
-              src="https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+              src={post.titleImageUrl}
               alt="TitleImage"
             />
           </div>
           <div className="BlogPostBody_Container">
-            <PostContent usage="title" mt="10px">
-              title
+            <PostContent usage="title" marginTop="10px">
+              {post.blogTitle}
             </PostContent>
             <div className="BlogPostInfo_Container">
-              <PostContent usage="info">2022년 02월 28일</PostContent>
+              <PostContent usage="info">{post.createdAt}</PostContent>
               <IconContainer
                 fontColor="var(--fc-300)"
                 iconColor="var(--fc-300)"
                 iconSize="var(--font-md)"
               >
                 <MdOutlineInsertComment className="Comment_icon" />
-                12
+                {post.commentCount}
               </IconContainer>
             </div>
           </div>
           <div className="BlogPostWriter_Container">
-            <PostContent usage="writer">글쓴이</PostContent>
+            <PostContent usage="writer">{post.writer}</PostContent>
             <IconContainer
               fontColor="var(--fc-500)"
               iconColor="var(--blue-400)"
               iconSize="var(--font-xl)"
             >
               <IoHeartCircle className="Heart_icon" />
-              12
+              {post.likeCount}
             </IconContainer>
           </div>
         </div>
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import BlogPost from "../Components/Common/BlogPost";
-import Header from "../Components/Common/Header/Header";
+
 
 const Home = () => {
   return (
     <div>
-      <BlogPost />
     </div>
   );
 };
