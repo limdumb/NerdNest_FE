0af882308e4da871ed0c4f112afb4b0dbc83ef6a
limdumb <dlaruddls@nate.com>
2023-03-01 23:14:01 +0900

    [Desgin] Blog Member Section Layout 설정

--- src/Pages/Blogs.tsx
+++ src/Pages/Blogs.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import MemberProfile from "../Components/Blogs/MemberProfile";
+import BlogRecord from "../Components/Blogs/BlogRecord";
+import BlogPost from "../Components/Blogs/BlogPost";
+import BlogCategory from "../Components/Blogs/BlogCategory";
 import styled from "styled-components";
+import "./Style/blogs.css";
 
 //추후 공용으로 뺄지는 상의예정
 export const Wrapper = styled.div`
@@ -14,14 +19,15 @@ const BlogWrapper = styled(Wrapper)`
 const Blogs = () => {
   return (
     <BlogWrapper>
-      <div>
-        <div></div>
-        <div></div>
+      <div className="Member_Information_Container">
+        <div className="Member_Profile_Container">
+          <MemberProfile />
+        </div>
+        <div className="Blog_Record_Container">
+          <BlogRecord />
+        </div>
       </div>
-      <div>
-        <div></div>
-        <div></div>
-      </div>
+      <div className="Blog_Information_Container"></div>
     </BlogWrapper>
   );
 };
