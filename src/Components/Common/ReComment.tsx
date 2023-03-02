7f6242f596c824da3c6abfd0fe4a82350f6d5c8f
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-03-02 18:45:31 +0900

    [Design] BlogDetail 기초 레이아웃 생성

---
+++ src/Components/Common/ReComment.tsx
@@ -0,0 +1,9 @@
+const ReComment = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ReComment;
--- src/Pages/BlogDetail.tsx
+++ src/Pages/BlogDetail.tsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useState } from "react";
+import BlogDetailComment from "../Components/Common/Comment";
+import "./Style/BlogDetail.css";
+
+interface BlogDetailProps {
+  blogTitle: string;
+  createdAt: string;
+  blogContents: string;
+  comments: [
+    {
+      commentId: number;
+      memberId: number;
+      nickName: string;
+      profileImageUrl: string;
+      comment: string;
+      createdAt: string;
+      modifiedAt: string;
+      perentsId: null | number;
+    }
+  ];
+}
 
 const BlogDetail = () => {
-    return (
-        <div>
-            
+  const [blogData, setBlogData] = useState<BlogDetailProps>();
+  return (
+    <div className="BlogDetail_Container">
+      <div className="BlogDetail_Title_Container">
+        <h1>제목</h1>
+        <div className="BlogDetail_Title_IM_Container">
+          <div className="BlogDetail_Title_Info">
+            <span>nickName</span>
+            <span>creaetedAt</span>
+            <span>modifiedAt</span>
+          </div>
+          <div className="BlogDetail_Title_Manage">
+            <button>수정</button>
+            <button>삭제</button>
+          </div>
         </div>
-    );
+      </div>
+      <div className="BlogDetail_Body_Container">{/* Contents */}</div>
+      <div className="BlogDetail_Comment_Container">
+        <h2>2 Comment</h2>
+        <BlogDetailComment />
+      </div>
+    </div>
+  );
 };
 
 export default BlogDetail;
