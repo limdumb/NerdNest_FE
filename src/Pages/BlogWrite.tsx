8b06c8a86fb270193a545f0f2b1c3ba23a3704b1
limdumb <dlaruddls@nate.com>
2023-03-06 13:14:27 +0900

    [Feat] Select 구현

--- src/Pages/BlogWrite.tsx
+++ src/Pages/BlogWrite.tsx
@@ -4,7 +4,8 @@ import { Wrapper } from "./Blogs";
 import TextEditor from "../Components/BlogWrite/TextEditor";
 import TitleImageUploarder from "../Components/BlogWrite/TitleImageUploarder";
 import CommonInput from "../Components/Common/CommonInput";
-import "./Style/blogWrite.css"
+import "./Style/blogWrite.css";
+import CategorySelect from "../Components/BlogWrite/CategorySelect";
 
 const WirteWrapper = styled(Wrapper)`
   margin-top: 20px;
@@ -20,14 +21,21 @@ const TitleWriteWrapper = styled(Wrapper)`
 `;
 
 const BlogWrite = () => {
-  const [blogText, setBlogText] = useState("")
-
+  const [blogText, setBlogText] = useState("");
   const [blogData, setBlogData] = useState({
     titleImageUrl: "",
     blogTitle: "",
     blogContent: "",
     categoryId: null,
   });
+  const [categoryValue, setCategoryValue] = useState(0)
+
+  const dummyData = [
+    { categoryId: 1, categoryName: "리액트" },
+    { categoryId: 2, categoryName: "리액트1" },
+    { categoryId: 3, categoryName: "리액트2" },
+    { categoryId: 4, categoryName: "리액트3" },
+  ];
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     e.persist();
@@ -36,6 +44,7 @@ const BlogWrite = () => {
       [e.target.name]: e.target.value,
     }));
   };
+  console.log(categoryValue)
 
   return (
     <WirteWrapper>
@@ -50,15 +59,20 @@ const BlogWrite = () => {
           name="blogTitle"
           label="H1"
           value={blogData.blogTitle}
-          onChange={(e: ChangeEvent<HTMLInputElement>) => {handleInputChange(e)}}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => {
+            handleInputChange(e);
+          }}
           type={"text"}
           placeholder={"제목을 입력하세요"}
         />
       </TitleWriteWrapper>
+      <label className="Editor_Label">Body</label>
       <div className="Text_Editor_Container">
-        <TextEditor blogText={blogText} setBlogText={setBlogText}/>
+        <TextEditor blogText={blogText} setBlogText={setBlogText} />
       </div>
-      <div></div>
+      <div>
+        <CategorySelect data={dummyData} setCategoryValue={setCategoryValue} />
+      </div>
       <div></div>
     </WirteWrapper>
   );
