1303367428f0054964e4995e64983514e524c0cb
limdumb <dlaruddls@nate.com>
2023-03-06 20:55:10 +0900

    [Feat] 중복체크 임시 API 생성

---
+++ src/API/Auth/Get/nickNameCheck.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export default function nickNameCheck(nickName: string) {
+  try {
+    axios.get(`/members/check?${nickName}`).then((res) => {
+      if (res.status === 200) {
+        alert("사용 가능한 닉네임 입니다 🙆🏻");
+      }
+    });
+  } catch (err) {
+    if (err === 409) {
+      alert("중복된 닉네임 입니다 다른 닉네임을 사용 해주세요 ❗️")
+    }
+  }
+}
