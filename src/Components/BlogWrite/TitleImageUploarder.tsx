dba55fac303938818e1c953b7f8aeb08da8a6335
limdumb <dlaruddls@nate.com>
2023-03-06 15:40:45 +0900

    [MISC] 주석 변경

--- src/Components/BlogWrite/TitleImageUploarder.tsx
+++ src/Components/BlogWrite/TitleImageUploarder.tsx
@@ -3,11 +3,7 @@ import "./Style/titleImageUploarder.css";
 interface Props{
   titleImageUrl: string
 }
-
-// image width , height 150px
-// 그 밑에 밋줄 긋기
-// 만약에 Edit이 true라면 이미지 밑에 + 아이콘 들어나게 하기
-
+//props 추후 내려올 예정
 export default function TitleImageUploarder() {
   return (
     <>
