8e7f121a8cba4f9b3eeb40dc5a2b7a99994e816e
limdumb <dlaruddls@nate.com>
2023-03-08 16:52:56 +0900

    [MISC] 불필요한 코드 삭제 및 주석추가

--- src/API/Auth/Post/login.ts
+++ src/API/Auth/Post/login.ts
@@ -1,4 +1,3 @@
-import { AxiosError } from "axios";
 import { NavigateFunction } from "react-router-dom";
 import { baseInstance } from "../../Instance/Instance";
 
@@ -7,7 +6,7 @@ interface Params {
   password: string;
   navigate: NavigateFunction;
 }
-//추후 추가예정
+
 interface LoginResponseType {
   memberId: number;
   accessToken: string;
@@ -29,7 +28,7 @@ export default async function login(params: Params) {
     }
   });
   try {
-    // err타입을 생각해볼 필요성 있음
+    // err 타입 핸들링 공부 예정
   } catch (err: any) {
     if (err.response.status === 401) {
       alert("로그인 정보가 잘못 되었습니다!");
