b4e1d41b3601a47a3bd55b9fbe7510c1c2f96a66
limdumb <dlaruddls@nate.com>
2023-03-08 20:10:32 +0900

    [Feat] 카테고리 수정및 추가버튼 조건부 랜더링 추가

--- src/Components/Blogs/BlogCategory.tsx
+++ src/Components/Blogs/BlogCategory.tsx
@@ -1,14 +1,33 @@
+import { useState } from "react";
 import { VscFolderOpened } from "react-icons/vsc";
-import "./Style/blogCategory.css"
+import { CategoryType } from "../../Pages/Blogs";
+import "./Style/blogCategory.css";
 
-export default function BlogCategory() {
+/*  
+  * 카테고리 CRUD는 memberId를 기준으로 확인하기 + API 요청은 전부 Token필요 *
+  1. 카테고리를 추가하는것
+  1-1 추가/수정/삭제 모드로 전환이 가능한 state를 만든다 + 버튼도 만든다 O
+  1-2 추가 버튼을 만든다 O
+  1-3 추가 버튼을 누르면 마지막 카테고리 밑에 input과 확인버튼을 만든다
+  1-4 확인 버튼을 누르면 추가되었습니다 라는 alert가 확인되면서
+      input과 버튼이 사라지고 카테고리가 추가된다
+  2. 카테고리를 삭제하는것
+  3. 카테고리를 수정하는것
+  4. 클릭할때 해당 카테고리에 맞는 데이터를 볼수있는 로직구현
+  4-1 url 변경시켜서 쿼리로 받을 수 있게 하기
+ */
+
+export default function BlogCategory(categoryList: CategoryType, editActive: boolean) {
   return (
     <ul>
-      <li className="Category_List">
-        <VscFolderOpened className="Folder_Icon"/>
-        <button className="Category_Name">전체</button>
-      </li>
-      {/* 추후 데이터 map예정 */}
+      {categoryList.categoryList?.map((el) => {
+        return (
+          <li className="Category_List" key={el.categoryId}>
+            <VscFolderOpened className="Folder_Icon" />
+            <button className="Category_Name">{el.categoryName}</button>
+          </li>
+        );
+      })}
     </ul>
   );
 }
--- src/Pages/Blogs.tsx
+++ src/Pages/Blogs.tsx
@@ -5,6 +5,11 @@ import BlogRecord from "../Components/Blogs/BlogRecord";
 import BlogPost from "../Components/Blogs/BlogPost";
 import BlogCategory from "../Components/Blogs/BlogCategory";
 import styled from "styled-components";
+import useFetch from "../Custom Hook/useFetch";
+import { useParams } from "react-router-dom";
+import { VscFolderOpened } from "react-icons/vsc";
+import { TiPen } from "react-icons/ti";
+import { HiPlusCircle } from "react-icons/hi";
 import "./Style/blogs.css";
 
 //추후 공용으로 뺄지는 상의예정
@@ -38,8 +43,15 @@ const BlogRecordWrapper = styled(Wrapper)`
   width: 70%;
 `;
 
+export type CategoryType = {
+  categoryList: { categoryId: number; categoryName: string }[] | undefined;
+};
+
 const Blogs = () => {
+  const params = useParams();
+  const memberId = localStorage.getItem("memberId");
   const [blogPosts, setBlogPosts] = useState<BlogPostType["blogList"]>([]);
+  const [editActive, setEditActive] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchBlogPosts = async () => {
@@ -47,9 +59,12 @@ const Blogs = () => {
       const response = await getBlogPost({ pages: 1, nickName: "가라" });
       setBlogPosts(response);
     };
-
     fetchBlogPosts();
   }, []);
+
+  const { data } = useFetch<CategoryType>(`/category/${params.memberId}`);
+  console.log(editActive);
+  //카테고리 => 멤버정보 => 블로그리스트 순서대로 데이터 받아볼것
   return (
     <BlogWrapper>
       <div className="Member_Information_Container">
@@ -69,8 +84,35 @@ const Blogs = () => {
       </div>
       <div className="Blog_Information_Container">
         <CategoryWrapper>
-          <h4>카테고리</h4>
-          <BlogCategory />
+          <div className="Category_Title_Container">
+            <div className="Category_Title">
+              <VscFolderOpened className="Category_Folder_Icon" />
+              <h3>카테고리</h3>
+            </div>
+            <div>
+              {editActive ? <HiPlusCircle /> : null}
+              {memberId === params.memberId ? (
+                editActive ? (
+                  <TiPen
+                    className="Category_Edit_Pen"
+                    onClick={() => {
+                      setEditActive(false);
+                      console.log("완료 로직");
+                    }}
+                  />
+                ) : (
+                  <TiPen
+                    className="Category_Edit_Pen"
+                    onClick={() => {
+                      console.log("수정ON");
+                      setEditActive(true);
+                    }}
+                  />
+                )
+              ) : null}
+            </div>
+          </div>
+          <BlogCategory categoryList={data?.categoryList} />
         </CategoryWrapper>
         <BlogPostWrapper>
           <BlogPost blogList={blogPosts} />
