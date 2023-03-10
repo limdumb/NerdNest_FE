45434a494354bd86e38664b277873c68c762393b
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-03-11 01:58:16 +0900

    [Feat] Query-String API 연결

--- src/Pages/Search.tsx
+++ src/Pages/Search.tsx
@@ -1,20 +1,44 @@
 import SearchInput from "../Components/Search/SearchInput";
-import BlogListDummy from "../API/DummyData/BlogList.json";
 import { useEffect, useState } from "react";
 import { ArrPostProps, BlogListContainer } from "./Home";
 import BlogPost from "../Components/Common/BlogPost";
+import { useSearchParams } from "react-router-dom";
+import getSearchData from "../API/Search/Get/getSearchData";
 import "./Style/Search.css";
 
 const Search = () => {
-  const [blogList, setBlogList] = useState<ArrPostProps>();
+  const [blogList, setBlogList] = useState<ArrPostProps>([
+    {
+      memberId: 0,
+      blogId: 0,
+      titleImageUrl: "",
+      blogTitle: "",
+      blogPreview: "",
+      createdAt: "",
+      writer: "",
+      commentCount: 0,
+      likeCount: 0,
+    },
+  ]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [scrollValue, setScrollValue] = useState(1);
+  const [searchParams] = useSearchParams();
+  const keyword = searchParams.get("keyword");
+
   useEffect(() => {
-    setBlogList(BlogListDummy.data);
-  }, []);
+    setIsLoading(true);
+    const get = async () => {
+      setIsLoading(false);
+      const result = await getSearchData(keyword, scrollValue);
+      setBlogList([...blogList, ...result]);
+    };
+    get();
+  }, [searchParams, scrollValue]);
 
   return (
     <div className="Search_Wrapper">
       <div className="Search_Container">
-        <SearchInput />
+        {keyword && <SearchInput keyword={keyword} />}
         <BlogListContainer>
           {blogList &&
             blogList.map((post) => <BlogPost key={post.blogId} post={post} />)}
