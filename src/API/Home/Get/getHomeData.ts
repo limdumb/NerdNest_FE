d01d3ee37813e9f0d1457fc1053a737271eb4866
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-03-11 01:24:52 +0900

    [MISC] 불필요한 주석 삭제

--- src/API/Home/Get/getHomeData.ts
+++ src/API/Home/Get/getHomeData.ts
@@ -1,13 +1,6 @@
 import { ArrPostProps } from "../../../Pages/Home";
 import { baseInstance } from "../../Instance/Instance";
 
-// export default function getHomeData(tabValue: string | null, page: number) {
-//   baseInstance
-//     .get(`/home/blogs?tab=${tabValue}&page=${page}`)
-//     .then((res) => res.data)
-//     .catch((err) => console.error(err));
-// }
-
 export default function getHomeData(
   tab: string | null,
   page: number
