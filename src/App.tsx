280d7058d507d162c98c782a00e3ceafe71d32a8
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-03-12 23:40:51 +0900

    Revert "[Feat] Search Query-String/무한스크롤"

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,14 @@
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import BlogDetail from "./Pages/BlogDetail";
-import BlogEdit from "./Pages/BlogEdit";
-import Blogs from "./Pages/Blogs";
-import BlogWrite from "./Pages/BlogWrite";
-import Home from "./Pages/Home";
-import Login from "./Pages/Login";
-import Search from "./Pages/Search";
-import SignUp from "./Pages/SignUp";
-import styled from "styled-components";
-import AuthPage from "./Pages/AuthPage";
-import Header from "./Components/Header/Header";
 import "./App.css";
-
-const RootContainer = styled.div`
-  width: 1200px;
-  height: 100%;
-  margin: 0 auto;
-`;
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Header />
-        <RootContainer>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/auth" element={<AuthPage />}>
-              <Route path="signup" element={<SignUp />} />
-              <Route path="login" element={<Login />} />
-            </Route>
-            <Route
-              path="/:nickName/:memberId/:category/:categoryId"
-              element={<Blogs />}
-            />
-            <Route path="/write" element={<BlogWrite />} />
-            <Route path="/edit/:blogId" element={<BlogEdit />} />
-            <Route
-              path="/:writer/:memberId/:title/:blogId"
-              element={<BlogDetail />}
-            />
-            <Route path="/search" element={<Search />} />
-          </Routes>
-        </RootContainer>
+        <Routes>
+          <Route />
+        </Routes>
       </BrowserRouter>
     </>
   );
