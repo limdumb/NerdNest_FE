36398856823145fde4dfe2f88d44b4ca60382184
limdumb <dlaruddls@nate.com>
2023-03-15 11:12:37 +0900

    [Refact] Kakao Login 로직 변경

--- src/Components/AuthPage/OauthButton.tsx
+++ src/Components/AuthPage/OauthButton.tsx
@@ -1,16 +1,34 @@
+import { useState } from "react";
+
 export const KakaoOauthButton = () => {
+  const [loading, setLoading] = useState(false);
+
+  const handleKakaoLogin = async () => {
+    setLoading(true);
+
+    try {
+      // 카카오 로그인 화면으로 이동하는 URL 생성
+      const redirectUri = encodeURIComponent(
+        "http://15.164.185.150:8080/login/oauth2/code/kakao"
+      ); // 백엔드 서버 주소를 입력
+      const clientId = "5afa29777ac5c75b6cd7d7eaacd0f5b6";
+      const url = `https://kauth.kakao.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code`;
+
+      // 카카오 로그인 화면으로 이동
+      window.open(url, `authForm`, "width=500, height=700");
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <>
       <button
         className="KaKao_Oauth_Button"
-        onClick={() => {
-          window.open(
-            "/oauth/kakao/login",
-            `authForm`,
-            "width=570, height=350"
-          );
-          // window.open(Kakao_Auth_Url, `authForm`, "width=570, height=350");
-        }}
+        onClick={handleKakaoLogin}
+        disabled={loading}
       >
         K
       </button>
--- src/Pages/OauthPage.tsx
+++ src/Pages/OauthPage.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import qs from "qs";
 
@@ -13,52 +13,30 @@ interface KakaoOAuthResponse {
 }
 
 export default function OauthPage() {
-  const Client_Secrit = "vwjdyiRlVjgnwN246cYGo03x1Mx0lmh0";
-  const Rest_API_Key = "5afa29777ac5c75b6cd7d7eaacd0f5b6";
-  const Redirect_Uri = "http://localhost:3000";
-  const Kakao_Auth_Url = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_API_Key}&redirect_uri=${Redirect_Uri}&response_type=code`;
-  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    window.location.href = Kakao_Auth_Url;
-  }, []);
+  const handleCallback = async () => {
+    setLoading(true);
 
-  useEffect(() => {
-    const code = new URLSearchParams(window.location.search).get("code");
-    if (code) {
-      axios
-        .post(
-          "https://kauth.kakao.com/oauth/token",
-          qs.stringify({
-            grant_type: "authorization_code",
-            client_id: Rest_API_Key,
-            redirect_uri: Redirect_Uri,
-            code: code,
-            client_secret: Client_Secrit,
-          }),
-          {
-            headers: {
-              "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
-            },
-          }
-        )
-        .then((res) => {
-          axios
-            .post("http://15.164.185.150:8080/login/oauth2/code/kakao", {
-              Authorization: res.data.id_token,
-            })
-            .then((res) => {
-              localStorage.setItem("memberId", res.data.memberId);
-              localStorage.setItem("accessToken", res.data.accessToken);
-              localStorage.setItem("refreshToken", res.data.refreshToken);
-              navigate("/")
-            });
-        })
-        .catch((error) => {
-          console.error("Failed to exchange auth code for access token", error);
-        });
+    try {
+      // 현재 URL에서 인가코드 추출
+      const authorizationCode = new URLSearchParams(window.location.search).get(
+        "code"
+      );
+
+      await axios.post("http://15.164.185.150:8080/login/oauth2/code/kakao", {
+        code: authorizationCode,
+      });
+
+      // // 액세스 토큰을 쿠키나 로컬 스토리지에 저장
+      // const accessToken = response.data.accessToken;
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
     }
-  }, []);
+  };
 
+  handleCallback();
   return <div>loading...</div>;
 }
