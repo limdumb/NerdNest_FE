81c41dc66a99ae5efa83a344efe7d1374596e11d
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-03-17 16:55:12 +0900

    [HOTFIX] DeleteComment 삭제 alert/confirm 추가

--- src/API/BlogDetail/Delete/deleteComment.ts
+++ src/API/BlogDetail/Delete/deleteComment.ts
@@ -1,10 +1,15 @@
 import { tokenInstance } from "../../Instance/Instance";
 
-export default function deleteComment(
-  commentId: number,
-) {
-  tokenInstance
-    .delete(`/comments/${commentId}`)
-    .then((res) => console.log(res))
-    .catch((err) => console.error(err));
+export default function deleteComment(commentId: number) {
+  if (window.confirm("정말 삭제하시겠습니까?")) {
+    tokenInstance
+      .delete(`/comments/${commentId}`)
+      .then((res) => {
+        if (res.status === 204) {
+          alert("삭제가 완료되었습니다.");
+          window.location.reload();
+        }
+      })
+      .catch((err) => console.error(err));
+  }
 }
