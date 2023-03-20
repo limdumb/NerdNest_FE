1fc367a51650bdc7e3e6c7c491697811738537dd
limdumb <dlaruddls@nate.com>
2023-03-20 20:58:17 +0900

    [Refact] 카테고리 수정로직 오류 해결

--- src/API/Blogs/Post/createCategory.ts
+++ src/API/Blogs/Post/createCategory.ts
@@ -1,26 +1,28 @@
+import { AxiosResponse } from "axios";
 import { tokenInstance } from "../../Instance/Instance";
 
-export default async function createCategory(
-  categoryName: string,
-) {
+export default async function createCategory(categoryName: string) {
   if (categoryName.length !== 0) {
-    if(categoryName !== "전체"){
+    if (categoryName !== "전체") {
       try {
-        await tokenInstance
-          .post("/category", { categoryName: categoryName })
-          .then((res) => {
-            if (res.status === 201) alert("카테고리가 생성 되었습니다.");
+        const response:AxiosResponse<{ categoryId: number; categoryName: string }> =
+          await tokenInstance.post("/category", {
+            categoryName: categoryName,
           });
-      } catch (err:any) {
-        if(err.response.status === 409){
-          alert("중복된 카테고리명 입니다.")
+
+        if (response.status === 201) {
+          alert("카테고리가 생성 되었습니다.");
+        }
+        return response.data.categoryId;
+      } catch (err: any) {
+        if (err.response === 409) {
+          alert("중복된 카테고리명 입니다.");
         }
       }
-    }else{
-      alert("전체 카테고리는 생성 할 수 없습니다.")
+    } else {
+      alert("전체 카테고리는 생성 할 수 없습니다.");
     }
-  }
-  else {
+  } else {
     alert("카테고리명을 입력해주세요!");
   }
 }
