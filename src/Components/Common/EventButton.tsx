d22e3afe3e71ae565695351a51bf438929223700
MINSE97 <99317339+MINSE97@users.noreply.github.com>
2023-03-20 17:18:58 +0900

    [Feat] InvalidPage 컴포넌트 생성

--- src/Components/Common/EventButton.tsx
+++ src/Components/Common/EventButton.tsx
@@ -2,7 +2,7 @@ import { ButtonHTMLAttributes } from "react";
 import styled from "styled-components";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  usage: "login" | "signUp" | "write" | "edit";
+  usage: string;
 }
 
 const CommonButton = styled.button<ButtonProps>`
@@ -52,7 +52,9 @@ export default function EventButton(props: ButtonProps) {
         ? "수정"
         : props.usage === "signUp"
         ? "회원가입"
-        : "작성"}
+        : props.usage === "write"
+        ? "작성"
+        : props.usage}
     </CommonButton>
   );
 }
